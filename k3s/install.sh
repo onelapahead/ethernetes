5561a646a36fdeedc6d8cbe4ef04ad6cf41f094d
W Hayden Fuss <wifu1234@gmail.com>
2021-02-14 15:02:08 -0500

    actual install script w/ docker

---
+++ k3s/install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+sudo -s
+
+cat <<EOF > /etc/rancher/k3s/config.yaml
+write-kubeconfig-mode: "0644"
+tls-san:
+  - "ethernetes.local"
+docker: true
+EOF
+
+curl -sfL https://get.k3s.io | sh -
+snap install helm --classic
+
+# ensure gpu-operator will work
+if [[ ! -e "/etc/modprobe.d/blacklist-nouveau.conf" ]]; then
+  cat <<EOF > /etc/modprobe.d/blacklist-nouveau.conf
+blacklist nouveau
+options nouveau modeset=0
+EOF
+  update-initramfs -u
+fi
+
+if ! cat /root/.bashrc | grep KUBECONFIG; then
+  cat <<EOF >> /root/.bashrc
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+alias k=kubectl
+alias oc=kubectl
+alias h=helm
+EOF
+fi
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+[[ -d "ethernetes/" ]] || git clone git@github.com/hfuss/etherenetes
+
+pushd ethernetes/k3s/
+  kubectl create ns e8s-system || true
+  helm upgrade --install \
+      argo-cd argo/argo-cd \
+      -n e8s-system \
+      -f argocd.yaml \
+      --atomic \
+      --wait
+popd
