21317b0b22e6ddd5dff985461e2744ab425de9f3
W Hayden Fuss <wifu1234@gmail.com>
2021-02-15 23:21:44 -0500

    tls

--- k3s/bootstrap.sh
+++ k3s/bootstrap.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 kubectl create ns e8s-system || true
 helm repo add argo https://argoproj.github.io/argo-helm
 helm upgrade --install \
-    argo-cd argo/argo-cd \
+    argocd argo/argo-cd \
     -n e8s-system \
     -f argocd.yaml \
     --atomic \
@@ -13,6 +15,13 @@ kubectl create ns cert-manager || true
 kubectl create ns kaleido || true
 kubectl create ns datadog || true
 
+
+set +e
 if ! kubectl get secret -n datadog datadog-api-creds 2> /dev/null; then
-  kubectl create secret ...
+  set -e
+  #kubectl create secret ...
+  echo "would made secrets"
 fi
+
+set -e
+
