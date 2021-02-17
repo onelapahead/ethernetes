f2fa6dff4ecfe6281d0608f2f1282d24372c4f04
W Hayden Fuss <wifu1234@gmail.com>
2021-02-17 09:34:55 -0500

    Refactoring GitOps

--- k3s/bootstrap.sh
+++ k3s/bootstrap.sh
@@ -5,12 +5,18 @@ set -e
 # ArgoCD
 kubectl create ns e8s-system || true
 helm repo add argo https://argoproj.github.io/argo-helm
-helm upgrade --install \
+
+set +e
+if ! helm get all argocd -n e8s-system > /dev/null; then
+  helm install \
     argocd argo/argo-cd \
     -n e8s-system \
     -f argocd.yaml \
-    --atomic \
     --wait
+
+  echo "sleeping for awhile to let apps sync and come up"
+  sleep 300
+fi
 
 # Secrets
 set +e
@@ -37,3 +43,12 @@ if ! kubectl get -n cert-manager secret clouddns-dns01-solver-svc-acct 2> /dev/n
 
 fi
 set -e
+
+helm upgrade --install \
+    argocd argo/argo-cd \
+    -n e8s-system \
+    -f argocd.yaml \
+    -f argocd-ingress-tls.yaml \
+    --atomic \
+    --wait
+
