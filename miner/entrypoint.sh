d79260d98d398c64d98ad302771d35b47f3a1854
W Hayden Fuss <wifu1234@gmail.com>
2021-02-18 22:19:02 -0500

    no need to specify tls version

--- miner/entrypoint.sh
+++ miner/entrypoint.sh
@@ -7,5 +7,5 @@ set +x # DEBUG
 # ensure SIGINTs get mapped to SIGTERMs
 exec dumb-init --rewrite 15:2 nsfminer \
   -U \
-  -P "stratum+tls12://${WALLET_ADDRESS}.${HOSTNAME}@${MINE_POOL}:${STRATUM_TLS_PORT}" \
+  -P "stratum+tls://${WALLET_ADDRESS}.${HOSTNAME}@${MINE_POOL}:${STRATUM_TLS_PORT}" \
   --api-port "-${API_PORT}" $@
