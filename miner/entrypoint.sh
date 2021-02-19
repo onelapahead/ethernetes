b26f8382ecce17a11ab859f5325f09deca8433f8
W Hayden Fuss <wifu1234@gmail.com>
2021-02-18 22:13:57 -0500

    debugging new image

--- miner/entrypoint.sh
+++ miner/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
+set +x # DEBUG
+
 # negative port number indicates API is in read-only mode preventing miner from being tampered with
 # ensure SIGINTs get mapped to SIGTERMs
 exec dumb-init --rewrite 15:2 nsfminer \
   -U \
-  -P stratum+tls12://${WALLET_ADDRESS}.${HOSTNAME}@${MINE_POOL}:${STRATUM_TLS_PORT} \
-  --api-port -${API_PORT} $@
+  -P "stratum+tls12://${WALLET_ADDRESS}.${HOSTNAME}@${MINE_POOL}:${STRATUM_TLS_PORT}" \
+  --api-port "-${API_PORT}" $@
