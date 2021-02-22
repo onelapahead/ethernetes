6c7a350449c2106ceb84d39b8310c05e9b7e0b0f
Hayden Fuss <wifu1234@gmail.com>
2021-02-21 22:36:18 -0500

    Using k3sup and Helm Controller for Install and Bootstrapping (#26)
    * Using k3sup and Helm Controller for Install and Bootstrapping
    
    * requirements

--- k3s/bootstrap-server.sh
+++ k3s/bootstrap-server.sh
@@ -2,28 +2,7 @@
 
 set -e
 
-# ArgoCD
-kubectl create ns e8s-system || true
-helm repo add argo https://argoproj.github.io/argo-helm > /dev/null
-
-set +e
-if ! helm get all argocd -n e8s-system > /dev/null; then
-  echo "Installing ArgoCD..."
-  helm install \
-    argocd argo/argo-cd \
-    --set server.extraArgs={--insecure} \
-    -n e8s-system \
-    -f argocd.yaml \
-    --wait
-
-  argoServer=$(kubectl get po -n e8s-system -l app.kubernetes.io/component=server -o jsonpath='{ ..metadata.name }')
-
-  kubectl exec \
-    -i --tty \
-    -n e8s-system ${argoServer} -- /bin/sh -c 'argocd login --insecure 127.0.0.1:8080 --username admin --password ${HOSTNAME} && argocd app sync bootstrap'
-  echo "Initial install complete. Copy /etc/rancher/k3s/k3s.yaml to the local machine and re-run bootstrap.sh to add secrets and Ingress secured with TLS."
-  exit 0
-fi
+hostname="${1:-ethernetes.brxblx.io}"
 
 # Secrets
 set +e
@@ -52,12 +31,6 @@ if ! kubectl get -n cert-manager secret clouddns-dns01-solver-svc-acct 2> /dev/n
 fi
 set -e
 
-echo "Managing ArgoCD..."
-helm upgrade --install \
-    argocd argo/argo-cd \
-    -n e8s-system \
-    -f argocd.yaml \
-    -f argocd-ingress-tls.yaml \
-    --atomic \
-    --wait
-
+echo "Updating ArgoCD..."
+scp config.yaml root@${hostname}:/etc/rancher/k3s/config.yaml
+scp bootstrap/helmchart-argocd.yaml root@${hostname}:/var/lib/rancher/k3s/server/manifests/helmchart-argocd.yaml
